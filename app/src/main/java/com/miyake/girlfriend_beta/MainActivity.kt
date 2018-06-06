5326795adabf7f06c1f35e549f0f27356e7399c3
Hiroki-Miyake <5bjk1222@mail.u-tokai.ac.jp>
2018-06-07 01:45:22 +0900

    バグ多いけど実装

--- app/src/main/java/com/miyake/girlfriend_beta/MainActivity.kt
+++ app/src/main/java/com/miyake/girlfriend_beta/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.miyake.girlfriend_beta
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.*
+import android.webkit.*
+import org.jetbrains.anko.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    var webView : WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        relativeLayout {
+
+            webView = webView {
+                loadUrl("http://vcard.ameba.jp/top")
+            }
+
+            // 外部ブラウザを立ちあげない
+            webView!!.setWebViewClient(object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                    return false
+                }
+            })
+
+            // javaScript を有効にする
+            webView!!.settings.javaScriptEnabled = true
+
+            // 各ボタン
+            linearLayout {
+                backgroundColor = Color.rgb(30, 150, 170)
+
+                // 再読み込み
+                button {
+                    setText("reload")
+
+                    onClick {
+                        webView!!.reload()
+                    }
+                }.lparams {
+                    width = dip(60)
+                    height = dip(60)
+                    margin = dip(5)
+                }
+
+                // テス勉結果
+                button {
+                    setText("resalt")
+
+                    onClick {
+                        webView!!.loadUrl("http://vcard.ameba.jp/s#study/battle/result")
+                    }
+                }.lparams {
+                    width = dip(60)
+                    height = dip(60)
+                    margin = dip(5)
+                }
+            }.lparams {
+                width = matchParent
+                height = dip(70)
+                alignParentBottom()
+            }
+        }
     }
 }
