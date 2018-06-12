c784c6d4ace63372429ad3974fb503a311a9b673
Hiroki-Miyake <5bjk1222@mail.u-tokai.ac.jp>
2018-06-12 19:43:08 +0900

    Ankoとxmlに分岐させただけ

--- app/src/main/java/com/miyake/girlfriend_beta/MainActivity.kt
+++ app/src/main/java/com/miyake/girlfriend_beta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.webkit.*
 import org.jetbrains.anko.*
 
@@ -15,103 +16,31 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         relativeLayout {
-
-            webView = webView {
-                loadUrl("http://vcard.ameba.jp/top")
-            }
-
-            // 外部ブラウザを立ちあげない
-//            webView.setWebViewClient(object : WebViewClient() {
-//                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-//                    return false
-//                }
-//            })
-            webView.webViewClient = object : WebViewClient(){}
-
-            // javaScript を有効にする
-            webView.settings.javaScriptEnabled = true
-
-
-            // 各ボタン
-            linearLayout {
-                backgroundColor = Color.rgb(30, 150, 170)
-
-                // blank
-                button {
-                    setText("GO")
-
-                    onClick {
-                        startActivity(Intent(this@MainActivity, XML_Activity::class.java))
-                    }
-                }.lparams {
-                    width = dip(60)
-                    height = dip(60)
-                    margin = dip(5)
-                    leftMargin = dip(15)
+            button {
+                setText("Anko")
+                onClick {
+                    startActivity(Intent(this@MainActivity, Anko_Activity::class.java))
                 }
-
-                // blank
-                button {
-                    setText("B")
-
-                    onClick {
-
-                    }
-                }.lparams {
-                    width = dip(60)
-                    height = dip(60)
-                    margin = dip(5)
-                    rightMargin = dip(15)
-                    leftMargin = dip(15)
-                }
-
-                // 再読み込み
-                button {
-                    setText("更新")
-
-                    onClick {
-                        webView.reload()
-                    }
-                }.lparams {
-                    width = dip(60)
-                    height = dip(60)
-                    margin = dip(5)
-                }
-
-                // テス勉結果
-                button {
-                    setText("結果")
-
-                    onClick {
-                        webView.loadUrl("http://vcard.ameba.jp/s#study/battle/result")
-                    }
-                }.lparams {
-                    width = dip(60)
-                    height = dip(60)
-                    margin = dip(5)
-                    rightMargin = dip(15)
-                    leftMargin = dip(15)
-                }
-
-                // blank
-                button {
-                    setText("C")
-
-                    onClick {
-
-                    }
-                }.lparams {
-                    width = dip(60)
-                    height = dip(60)
-                    margin = dip(5)
-                    rightMargin = dip(10)
-                }
-
             }.lparams {
-                width = matchParent
-                height = dip(70)
-                alignParentBottom()
+                width = dip(100)
+                height = dip(50)
+                alignParentTop()
+                topMargin = dip(150)
+                gravity = Gravity.CENTER_HORIZONTAL
             }
+
+            button {
+                setText("xml")
+                onClick {
+                    startActivity(Intent(this@MainActivity, XML_Activity::class.java))
+                }
+            }.lparams {
+                width = dip(100)
+                height = dip(50)
+                topMargin = dip(250)
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
+
         }
     }
 }
